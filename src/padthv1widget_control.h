5698ab537ba878b428185b8d5fb84b5dc9238d31
rncbc <rncbc@rncbc.org>
2017-12-28 22:39:47 +0000

    - An anti-flooding timer is now in place in MIDI Controller   assignment (aka. MIDI learn) dialog. (EXPERIMENTAL)

--- src/padthv1widget_control.h
+++ src/padthv1widget_control.h
@@ -34,6 +34,8 @@ namespace Ui { class padthv1widget_control; }
 class QAbstractButton;
 class QCloseEvent;
 
+class QTime;
+
 
 //----------------------------------------------------------------------------
 // padthv1widget_control -- UI wrapper form.
@@ -122,6 +124,9 @@ private:
 
 	int m_iDirtyCount;
 	int m_iDirtySetup;
+
+	// Anti-flooding guard timer.
+	QTime *m_pEventTimer;
 
 	// Pseudo-singleton instance.
 	static padthv1widget_control *g_pInstance;

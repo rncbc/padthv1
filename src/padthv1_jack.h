12e6422e7f96eb3969d29b633f39adfb34b78a5a
rncbc <rncbc@rncbc.org>
2018-08-14 11:35:16 +0100

    - SIGTERM (and SIGINT) signal handler added to close the JACK   stand-alone client applications properly. (EXPERIMENTAL)

--- src/padthv1_jack.h
+++ src/padthv1_jack.h
@@ -115,6 +115,9 @@ class padthv1widget_jack;
 class padthv1_nsm;
 #endif
 
+#ifdef HAVE_SIGNAL_H
+class QSocketNotifier;
+#endif
 
 class padthv1_jack_application : public QObject
 {
@@ -144,6 +147,11 @@ protected slots:
 
 #endif	// CONFIG_NSM
 
+#ifdef HAVE_SIGNAL_H
+	// SIGTERM signal handler.
+	void sigterm_handler();
+#endif
+
 protected:
 
 	// Argument parser method.
@@ -164,6 +172,10 @@ private:
 
 #ifdef CONFIG_NSM
 	padthv1_nsm *m_pNsmClient;
+#endif
+
+#ifdef HAVE_SIGNAL_H
+	QSocketNotifier *m_pSigtermNotifier;
 #endif
 };
 

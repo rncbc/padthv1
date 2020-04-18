82668b95ac9b1bdbc04e72cf4df5428ad0dca932
rncbc <rncbc@rncbc.org>
2020-04-18 16:16:04 +0100

    - LV2 patch:Put has been replaced by a series of patch:Set messages. (EXPERIMENTAL)

--- src/padthv1_lv2.cpp
+++ src/padthv1_lv2.cpp
@@ -152,10 +152,6 @@ padthv1_lv2::padthv1_lv2 (
 					m_urid_map->handle, LV2_PATCH__Get);
 				m_urids.patch_Set = m_urid_map->map(
 					m_urid_map->handle, LV2_PATCH__Set);
-				m_urids.patch_Put = m_urid_map->map(
-					m_urid_map->handle, LV2_PATCH__Put);
-				m_urids.patch_body = m_urid_map->map(
-					m_urid_map->handle, LV2_PATCH__body);
 				m_urids.patch_property = m_urid_map->map(
 					m_urid_map->handle, LV2_PATCH__property);
 				m_urids.patch_value = m_urid_map->map(
@@ -347,7 +343,7 @@ void padthv1_lv2::run ( uint32_t nframes )
 				else
 				if (object->body.otype == m_urids.patch_Get) {
 					// put all property values (probably to UI)
-					patch_put();
+					patch_get();
 				}
 			#endif	// CONFIG_LV2_PATCH
 			}
@@ -672,7 +668,7 @@ bool padthv1_lv2::worker_response ( const void *data, uint32_t size )
 		return state_changed();
 
 #ifdef CONFIG_LV2_PATCH
-	return patch_put();
+	return patch_get();
 #else
 	return true;
 #endif
@@ -693,51 +689,55 @@ bool padthv1_lv2::state_changed (void)
 
 #ifdef CONFIG_LV2_PATCH
 
-bool padthv1_lv2::patch_put ( uint32_t type )
+bool padthv1_lv2::patch_set ( LV2_URID key )
 {
 	static char s_szNull[1] = {'\0'};
-
-	if (type == m_urids.patch_Put)
-		type = 0;
 
 	lv2_atom_forge_frame_time(&m_forge, m_ndelta);
 
 	LV2_Atom_Forge_Frame patch_frame;
-	lv2_atom_forge_object(&m_forge, &patch_frame, 0, m_urids.patch_Put);
-	lv2_atom_forge_key(&m_forge, m_urids.patch_body);
+	lv2_atom_forge_object(&m_forge, &patch_frame, 0, m_urids.patch_Set);
 
-	LV2_Atom_Forge_Frame body_frame;
-	lv2_atom_forge_object(&m_forge, &body_frame, 0, 0);
+	lv2_atom_forge_key(&m_forge, m_urids.patch_property);
+	lv2_atom_forge_urid(&m_forge, key);
 
-	if (type == 0 || type == m_urids.p201_tuning_enabled) {
-		lv2_atom_forge_key(&m_forge, m_urids.p201_tuning_enabled);
+	lv2_atom_forge_key(&m_forge, m_urids.patch_value);
+
+	if (key == m_urids.p201_tuning_enabled)
 		lv2_atom_forge_bool(&m_forge, padthv1::isTuningEnabled());
-	}
-	if (type == 0 || type == m_urids.p202_tuning_refPitch) {
-		lv2_atom_forge_key(&m_forge, m_urids.p202_tuning_refPitch);
+	else
+	if (key == m_urids.p202_tuning_refPitch)
 		lv2_atom_forge_float(&m_forge, padthv1::tuningRefPitch());
-	}
-	if (type == 0 || type == m_urids.p203_tuning_refNote) {
-		lv2_atom_forge_key(&m_forge, m_urids.p203_tuning_refNote);
+	else
+	if (key == m_urids.p203_tuning_refNote)
 		lv2_atom_forge_int(&m_forge, padthv1::tuningRefNote());
-	}
-	if (type == 0 || type == m_urids.p204_tuning_scaleFile) {
+	else
+	if (key == m_urids.p204_tuning_scaleFile) {
 		const char *pszScaleFile = padthv1::tuningScaleFile();
 		if (pszScaleFile == nullptr)
 			pszScaleFile = s_szNull;
-		lv2_atom_forge_key(&m_forge, m_urids.p204_tuning_scaleFile);
 		lv2_atom_forge_path(&m_forge, pszScaleFile, ::strlen(pszScaleFile) + 1);
 	}
-	if (type == 0 || type == m_urids.p205_tuning_keyMapFile) {
+	else
+	if (key == m_urids.p205_tuning_keyMapFile) {
 		const char *pszKeyMapFile = padthv1::tuningKeyMapFile();
 		if (pszKeyMapFile == nullptr)
 			pszKeyMapFile = s_szNull;
-		lv2_atom_forge_key(&m_forge, m_urids.p205_tuning_keyMapFile);
 		lv2_atom_forge_path(&m_forge, pszKeyMapFile, ::strlen(pszKeyMapFile) + 1);
 	}
 
-	lv2_atom_forge_pop(&m_forge, &body_frame);
 	lv2_atom_forge_pop(&m_forge, &patch_frame);
+
+	return true;
+}
+
+bool padthv1_lv2::patch_get (void)
+{
+	patch_set(m_urids.p201_tuning_enabled);
+	patch_set(m_urids.p202_tuning_refPitch);
+	patch_set(m_urids.p203_tuning_refNote);
+	patch_set(m_urids.p204_tuning_scaleFile);
+	patch_set(m_urids.p205_tuning_keyMapFile);
 
 	return true;
 }

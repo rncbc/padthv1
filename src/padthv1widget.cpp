fe88680f4432db0c950b6f8817d8e27a4a76fda2
rncbc <rncbc@rncbc.org>
2020-06-20 23:21:07 +0100

    - A long awaited expedite Panic button is now featured.

--- src/padthv1widget.cpp
+++ src/padthv1widget.cpp
@@ -548,6 +548,11 @@ padthv1widget::padthv1widget ( QWidget *pParent )
 		SIGNAL(sampleChanged()),
 		SLOT(resetSample2()));
 
+	// Randomize params...
+	QObject::connect(m_ui.PanicButton,
+		SIGNAL(clicked()),
+		SLOT(panic()));
+	
 	// Direct stacked-page signal/slot
 	QObject::connect(m_ui.TabBar, SIGNAL(currentChanged(int)),
 		m_ui.StackedWidget, SLOT(setCurrentIndex(int)));
@@ -905,6 +910,15 @@ void padthv1widget::swapParams ( bool bOn )
 	const bool bSwapA = m_ui.SwapParamsAButton->isChecked();
 	m_ui.StatusBar->showMessage(tr("Swap %1").arg(bSwapA ? 'A' : 'B'), 5000);
 	updateDirtyPreset(true);
+}
+
+
+// Panic: all-notes/sound-off (reset).
+void padthv1widget::panic (void)
+{
+	padthv1_ui *pSynthUi = ui_instance();
+	if (pSynthUi)
+		pSynthUi->reset();
 }
 
 

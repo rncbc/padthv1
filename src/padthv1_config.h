f4e1ab5de8f0ae52848dc9f13918ea5844c150cf
rncbc <rncbc@rncbc.org>
2023-03-22 10:21:00 +0000

    - Preset list/cache accessor optimization.

--- src/padthv1_config.h
+++ src/padthv1_config.h
@@ -95,7 +95,7 @@ public:
 	QString presetFile(const QString& sPreset);
 	void setPresetFile(const QString& sPreset, const QString& sPresetFile);
 	void removePreset(const QString& sPreset);
-	QStringList presetList();
+	const QStringList& presetList();
 
 	// Programs utility methods.
 	void loadPrograms(padthv1_programs *pPrograms);
@@ -127,6 +127,9 @@ protected:
 	void save();
 
 private:
+
+	// The presets list cache.
+	QStringList m_presetList;
 
 	// The singleton instance.
 	static padthv1_config *g_pSettings;

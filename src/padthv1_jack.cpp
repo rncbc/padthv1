138cc122620ca2aca682e87b94657aa78d24d7f5
rncbc <rncbc@rncbc.org>
2023-11-04 23:24:30 +0000

    - Replace foreach() macros with C++11 for() constructs. (REGRESSION)

--- src/padthv1_jack.cpp
+++ src/padthv1_jack.cpp
@@ -881,7 +881,7 @@ bool padthv1_jack_application::parse_args (void)
 		m_sClientName = sVal;
 	}
 
-	for (const QString& sArg : parser.positionalArguments()) {
+	foreach (const QString& sArg, parser.positionalArguments()) {
 		m_presets.append(sArg);
 	}
 

27b3c5f1c52cedf17cd059adf0789f8a564f7a75
rncbc <rncbc@rncbc.org>
2024-09-03 11:18:45 +0100

    - NSM: take proper preset name from session on save.

--- src/padthv1_jack.cpp
+++ src/padthv1_jack.cpp
@@ -1105,9 +1105,9 @@ void padthv1_jack_application::saveSession (void)
 
 //	const QString& client_name = m_pNsmClient->client_name();
 	const QString& path_name = m_pNsmClient->path_name();
-//	const QString& display_name = m_pNsmClient->display_name();
-//	const QFileInfo fi(path_name, display_name + '.' + PROJECT_NAME);
-	const QFileInfo fi(path_name, "session." PROJECT_NAME);
+	const QString& display_name = m_pNsmClient->display_name();
+	const QFileInfo fi(path_name, display_name + '.' + PROJECT_NAME);
+//	const QFileInfo fi(path_name, "session." PROJECT_NAME);
 
 	const bool bSave
 		= padthv1_param::savePreset(m_pPadth, fi.absoluteFilePath(), true);

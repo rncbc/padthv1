b6814c9c939669dbfecb653594bc8f7571bd1b2d
rncbc <rncbc@rncbc.org>
2026-08-18 09:42:51 +0100

    - Make sure all sync'ed to disk when accepting new Help/Configure.../Presets.

--- src/padthv1_config.h
+++ src/padthv1_config.h
@@ -94,6 +94,10 @@ public:
 	void setPresetFile(const QString& sPreset, const QString& sPresetFile);
 	void removePreset(const QString& sPreset);
 
+	// Presets utility methods.
+	void loadPresets();
+	void savePresets();
+
 	// Programs utility methods.
 	void loadPrograms(padthv1_programs *pPrograms);
 	void savePrograms(padthv1_programs *pPrograms);

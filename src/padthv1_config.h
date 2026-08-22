485ee502e2bfcd3bb66e6a9b3da51757e01707a9
rncbc <rncbc@rncbc.org>
2026-08-22 10:01:11 +0100

    - Bank/Presets file import/export introduced, now honoring their   respective absolute/relative preset file paths. (EXPERIMENTAL)

--- src/padthv1_config.h
+++ src/padthv1_config.h
@@ -123,12 +123,21 @@ protected:
 	static QString presetsBanksGroup();
 	static QString presetsBankListKey();
 
+	// Absolute/relative path functors.
+	struct MapPath
+	{
+		virtual QString operator()(const QString& sPath) const
+			{ return sPath; }
+	};
+
 	static void loadPresets(
 		QSettings *pSettings,
-		padthv1_presets *pPresets);
+		padthv1_presets *pPresets,
+		const MapPath& mapPath = MapPath());
 	static void savePresets(
 		QSettings *pSettings,
-		padthv1_presets *pPresets);
+		padthv1_presets *pPresets,
+		const MapPath& mapPath = MapPath());
 
 	// Banks programs group path.
 	static QString programsGroup();
